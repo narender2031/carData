b9418bb25dcade4f8dfb2ca85a15e46ffb2745ad
Narender Singh <narender2031@gmail.com>
2021-02-01 00:40:29 +0530

    added UI package

--- src/Main.tsx
+++ src/Main.tsx
@@ -11,15 +11,20 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import HomeScreen from './Screens/Home';
+import {Provider as PaperProvider} from 'react-native-paper';
+import Theme from './utils/theme.config';
+
 declare const global: {HermesInternal: null | {}};
 
 const Mian = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <HomeScreen />
-      </SafeAreaView>
+      <PaperProvider theme={Theme}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <HomeScreen />
+        </SafeAreaView>
+      </PaperProvider>
     </>
   );
 };
